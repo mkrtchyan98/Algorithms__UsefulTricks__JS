4bdc98f6a5d60819ecb2fe32abad242a5580974b
Tigran <50393947+mkrtchyan98@users.noreply.github.com>
2021-03-24 14:17:05 +0400

    Update composeAndPipe.js

--- Useful_Tricks/composeAndPipe.js
+++ Useful_Tricks/composeAndPipe.js
@@ -66,4 +66,12 @@ function emptyUserCart(user) {
   history1.push(user)
   return Object.assign({}, user, { cart: [] });
 }
+///another example of pipe
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
+const fn1 = s => s.toLowerCase();
+const fn2 = s => s.split('').reverse().join('');
+const fn3 = s => s + '!'
+
+const newFunc = pipe(fn1, fn2, fn3);
+const result = newFunc('Time'); // emit!
