282f1b76f711f295659333ed66a991d7df86379a
Tigran <50393947+mkrtchyan98@users.noreply.github.com>
2021-06-25 13:49:53 +0400

    Create wordCounter.js

---
+++ Useful_Tricks/wordCounter.js
@@ -0,0 +1,9 @@
+function wordCount( val ){
+  var wom = val.match(/\S+/g);
+  return{    
+    charactersNoSpaces : val.replace(/\s+/g,'').length,
+    characters         : val.length, 
+    words              : wom ? wom.length:0,
+    lines              : val.split(/\r*\n/).length
+  };
+}
