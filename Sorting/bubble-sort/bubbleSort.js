7deecab91f8a9467143e3bed8894bfe7fab681e6
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2021-09-07 16:51:28 +0400

    add selection_sort

--- Sorting/bubble-sort/bubbleSort.js
+++ Sorting/bubble-sort/bubbleSort.js
@@ -1,14 +1,12 @@
 function bubbleSort(arr) {
-    let count = 0;
-    for(let j=0;j<arr.length;j++) {
-        for(let i = 0; i < arr.length; i++) {
-              if(arr[i] > arr[i+1]) {
-                 [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-                 count++
-              }
-          }
+  let count = 0;
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        count++;
       }
-      return arr;
-
+    }
+  }
+  return arr;
 }
-
