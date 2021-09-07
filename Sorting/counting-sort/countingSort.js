9d028539de0c2d9fea7f4f68b35828dc39dba8d1
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2021-09-07 17:45:25 +0400

    update count

--- Sorting/counting-sort/countingSort.js
+++ Sorting/counting-sort/countingSort.js
@@ -1,17 +1,18 @@
-function countingSort(arr) {
-  let count = new Array(Math.max(...arr));
-  console.log("count", count);
-  for (let i = 0; i < arr.length; i++) {
-    //Count the frequency of each element in the original array
-    //And store it in  array
-    ++count[arr[i]];
-    let index = 0;
-    for (let j = 0; j < count.length; j++) {
-      for (let k = 0; k < count[i]; k++) {
-        arr[index++] = i;
-      }
-    }
-  }
-  return arr;
-}
-
+const arr = [4, 3, 1, 2, 3];
+const findMaximum = arr => arr.reduce((acc, val) => val > acc ? val: acc, Number.MIN_VALUE)
+const countingSort = (arr = []) => {
+   const max = Math.max(...arr);
+   const counts = new Array(max + 1);
+   counts.fill(0);
+   arr.forEach(value => counts[value]++);
+   const res = [];
+   let resultIndex = 0;
+   counts.forEach((count, index) => {
+      for (let i = 0; i < count; i++) {
+         res[resultIndex] = index;
+         resultIndex++;
+      };
+   });
+   return res;
+};
+console.log(countingSort(arr));
