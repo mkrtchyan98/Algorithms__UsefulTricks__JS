a007d80339750bc9fbc8c20abf966a4df6056aa3
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2021-09-07 16:40:17 +0400

    add bubble sort

---
+++ Sorting/bubble-sort/bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    let count = 0;
+    for(let j=0;j<arr.length;j++) {
+        for(let i = 0; i < arr.length; i++) {
+              if(arr[i] > arr[i+1]) {
+                 [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                 count++
+              }
+          }
+      }
+      return arr;
+
+}
+
